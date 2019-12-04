3efba8d8aa6f8b0ab0c7ed9b487ec2f0954b7cf3
raphaels <raphael.schib@kara-network.ch>
2019-12-04 23:04:53 +0100

    added script

---
+++ BUZZER/BUZZER.ino
@@ -0,0 +1,203 @@
+
+#include <Arduino.h>
+#include <SPI.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+#include "keycode.h"
+#include "OneButton.h"
+
+#if SOFTWARE_SERIAL_AVAILABLE
+  #include <SoftwareSerial.h>
+#endif
+
+/*=========================================================================
+    APPLICATION SETTINGS
+
+    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
+   
+                              Enabling this will put your Bluefruit LE module
+                              in a 'known good' state and clear any config
+                              data set in previous sketches or projects, so
+                              running this at least once is a good idea.
+   
+                              When deploying your project, however, you will
+                              want to disable factory reset by setting this
+                              value to 0.  If you are making changes to your
+                              Bluefruit LE device via AT commands, and those
+                              changes aren't persisting across resets, this
+                              is the reason why.  Factory reset will erase
+                              the non-volatile memory where config data is
+                              stored, setting it back to factory default
+                              values.
+       
+                              Some sketches that require you to bond to a
+                              central device (HID mouse, keyboard, etc.)
+                              won't work at all with this feature enabled
+                              since the factory reset will clear all of the
+                              bonding data stored on the chip, meaning the
+                              central device won't be able to reconnect.
+    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
+    -----------------------------------------------------------------------*/
+    #define FACTORYRESET_ENABLE         0
+    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+/*=========================================================================*/
+
+
+// Create the bluefruit object, either software serial...uncomment these lines
+/*
+/* ...or hardware serial, which does not need the RTS/CTS pins. Uncomment this line */
+// Adafruit_BluefruitLE_UART ble(BLUEFRUIT_HWSERIAL_NAME, BLUEFRUIT_UART_MODE_PIN);
+
+/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+/* ...software SPI, using SCK/MOSI/MISO user-defined SPI pins and then user selected CS/IRQ/RST */
+//Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_SCK, BLUEFRUIT_SPI_MISO,
+//                             BLUEFRUIT_SPI_MOSI, BLUEFRUIT_SPI_CS,
+//                             BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+typedef struct
+{
+  uint8_t modifier;   /**< Keyboard modifier keys  */
+  uint8_t reserved;   /**< Reserved for OEM use, always set to 0. */
+  uint8_t keycode[6]; /**< Key codes of the currently pressed keys. */
+} hid_keyboard_report_t;
+
+// Report that send to Central every scanning period
+hid_keyboard_report_t keyReport = { 0, 0, { 0 } };
+
+// Report sent previously. This is used to prevent sending the same report over time.
+// Notes: HID Central intepretes no new report as no changes, which is the same as
+// sending very same report multiple times. This will help to reduce traffic especially
+// when most of the time there is no keys pressed.
+// - Init to different with keyReport
+hid_keyboard_report_t previousReport = { 0, 0, { 1 } };
+
+int inputKeycodes[6];
+
+OneButton button(A0, true);
+
+unsigned long langegGedrueckt = 4000;
+unsigned long buttonPushedMillis;
+
+/**************************************************************************/
+/*!
+    @brief  Sets up the HW an the BLE module (this function is called
+            automatically on startup)
+*/
+/**************************************************************************/
+void setup(void)
+{
+  //while (!Serial);  // required for Flora & Micro
+  delay(500);
+
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit HID Keyboard Example"));
+  Serial.println(F("---------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE )
+  {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ){
+      error(F("Couldn't factory reset"));
+    }
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  /* Change the device name to make it easier to find */
+  Serial.println(F("Setting device name to 'Bluefruit Keyboard': "));
+  if (! ble.sendCommandCheckOK(F( "AT+GAPDEVNAME=Bluefruit Keyboard" )) ) {
+    error(F("Could not set device name?"));
+  }
+
+  /* Enable HID Service */
+  Serial.println(F("Enable HID Service (including Keyboard): "));
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  {
+    if ( !ble.sendCommandCheckOK(F( "AT+BleHIDEn=On" ))) {
+      error(F("Could not enable Keyboard"));
+    }
+  }else
+  {
+    if (! ble.sendCommandCheckOK(F( "AT+BleKeyboardEn=On"  ))) {
+      error(F("Could not enable Keyboard"));
+    }
+  }
+
+  /* Add or remove service requires a reset */
+  Serial.println(F("Performing a SW reset (service changes require a reset): "));
+  if (! ble.reset() ) {
+    error(F("Couldn't reset??"));
+  }
+
+  Serial.println();
+  Serial.println(F("Go to your phone's Bluetooth settings to pair your device"));
+  Serial.println(F("then open an application that accepts keyboard input"));
+
+  button.attachLongPressStop(longclick);  
+  button.attachClick(singleclick); 
+  
+}
+
+
+void loop(void)
+{
+  button.tick();
+  delay(10);
+}
+
+
+void singleclick(){ // what happens when the button is clicked
+  Serial.println( F("kurz") );
+  
+  //define key
+  inputKeycodes[0] = HID_KEY_S;
+  keyReport.keycode[0] = inputKeycodes[0];
+
+  //send button
+  ble.atcommand("AT+BLEKEYBOARDCODE", (uint8_t*) &keyReport, 8);
+  //clear the connection
+  ble.atcommand("AT+BleKeyboardcode", "00-00-00-00-00-00-00-00");
+  Serial.println( keyReport.keycode[0] );             
+}
+ 
+void longclick(){ // what happens when button is long-pressed
+  Serial.println( F("lange") );
+
+  //define key
+  inputKeycodes[0] = HID_KEY_C;
+  keyReport.keycode[0] = inputKeycodes[0];
+  //send button
+  ble.atcommand("AT+BLEKEYBOARDCODE", (uint8_t*) &keyReport, 8);
+  //clear the connection
+  ble.atcommand("AT+BleKeyboardcode", "00-00-00-00-00-00-00-00");
+}
---
+++ LED_STRIP/LED_STRIP.ino
@@ -0,0 +1,312 @@
+ 
+#include <ESP8266WiFi.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN    D4
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 94
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+// ADD SSID AND PASSWORD!!!!
+// *****************************
+const char* ssid = "";
+const char* password = "";
+ 
+int ledPin = D5;
+
+WiFiServer server(80);
+IPAddress ip(x,x,x,x); // where xx is the desired IP Address
+IPAddress gateway(255, 255, 255, 0); // set gateway to match your network
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+ 
+ 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+ 
+  Serial.print(F("Setting static ip to : "));
+  Serial.println(ip);
+ 
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  IPAddress subnet(255, 255, 255, 0); // set subnet mask to match your network
+  WiFi.config(ip, gateway, subnet); 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+ 
+  // Print the IP address
+  Serial.print("Use this URL : ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+
+   //LED STRIP
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+   #endif
+  // END of Trinket-specific code.
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(150); // Set BRIGHTNESS to about 1/5 (max = 255)
+}
+ 
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+ 
+  // Wait until the client sends some data
+  
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+ 
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+ 
+  // Match the request
+ 
+  int value = LOW;
+  if (request.indexOf("/COUNTDOWN") != -1) {
+  Serial.println("COUNTDOWN");
+   countdown(strip.Color(255,   255,   255), 1000); // Red
+  } 
+  if (request.indexOf("/BACK") != -1) {
+    Serial.println("BACK");
+   back(strip.Color(0,   0,   0), 1000); // Red
+  } 
+  if (request.indexOf("/THREE") != -1) {
+    Serial.println("BACKTHREE");
+   backthree(strip.Color(0,   0,   0), 1000); // Red
+  } 
+    if (request.indexOf("/TWO") != -1) {
+    Serial.println("TWO");
+   backtwo(strip.Color(0,   0,   0), 1000); // Red
+  } 
+  if (request.indexOf("/COLLAGE") != -1){
+    Serial.println("COLLAGE");
+   
+   theaterChaseRainbow(30);
+  }
+    if (request.indexOf("/PIC") != -1){
+    Serial.println("PIC");
+   rainbow(4);
+  }
+ 
+ 
+ 
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+ 
+  client.println("<br><br>");
+  client.println("Click <a href=\"/COUNTDOWN\">here</a> to start the countdown<br>");
+  client.println("Click <a href=\"/BACK\">here</a> to start the countdownback<br>");
+  client.println("Click <a href=\"/THREE\">here</a> to start the countdownback<br>");
+  client.println("Click <a href=\"/TWO\">here</a> to start the countdownback<br>");
+  client.println("Click <a href=\"/COLLAGE\">here</a> to start collage<br>");
+  client.println("Click <a href=\"/PIC\">here</a> to start pic<br>");
+  client.println("</html>");
+ 
+  delay(1);
+  Serial.println("Client disconnected");
+  Serial.println("");
+}
+
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+  }
+}
+
+void countdown(uint32_t color, int wait){
+  
+  strip.clear();
+  int p=83;
+  
+  for(int i=2;i<strip.numPixels();) { // For each pixel in strip...
+    strip.fill(color,i,9);
+    strip.fill(color,p,9);
+    strip.show(); //  Update strip to match
+    if (i>=47){
+      delay(100);
+      break;
+    }
+      
+    p = p - 9; //  Pause for a moment
+    i = i + 9;
+    delay(wait);
+  } 
+  stripClear();
+}
+
+
+void back(uint32_t color, int wait){
+
+  strip.fill(strip.Color(255,   255,   255),2,90);
+  strip.show();
+  delay(1000);
+  
+  int p=38;
+ 
+  for(int i=47;i<strip.numPixels();) { // For each pixel in strip...
+    strip.fill(color,i,9);
+    strip.fill(color,p,9);
+
+    strip.show();                          //  Update strip to match
+    if (i>=76){
+      delay(100);
+      break;
+    }
+
+    p = p - 9; 
+    i = i + 9;
+    delay(wait); 
+  } 
+  stripClear();
+  
+}
+
+void backthree(uint32_t color, int wait){
+
+  strip.fill(strip.Color(255,   255,   255),2,90);
+  strip.show();
+  delay(1000);
+  
+  int p=32;
+ 
+  for(int i=47;i<strip.numPixels();) { // For each pixel in strip...
+    strip.fill(color,i,15);
+    strip.fill(color,p,15);
+
+    strip.show();                          //  Update strip to match
+    if (i>=76){
+      delay(100);
+      break;
+    }
+
+    p = p - 15; 
+    i = i + 15;
+    delay(wait); 
+  } 
+  stripClear();
+  
+}
+
+void backtwo(uint32_t color, int wait){
+
+  strip.fill(strip.Color(255,   255,   255),2,91);
+  strip.show();
+  delay(1000);
+  
+  int p=24;
+ 
+  for(int i=47;i<strip.numPixels();) { // For each pixel in strip...
+    strip.fill(color,i,23);
+    strip.fill(color,p,23);
+
+    strip.show();                          //  Update strip to match
+    if (i>=70){
+      delay(100);
+      break;
+    }
+
+    p = p - 23; 
+    i = i + 23;
+    delay(wait); 
+    Serial.println(p);
+    Serial.println(i);
+  } 
+  stripClear();
+  
+}
+
+void rainbow(int wait) {
+   strip.clear();
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
+  for(long firstPixelHue = 0; firstPixelHue < 1*65536; firstPixelHue += 256) {
+    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the strip
+      // (strip.numPixels() steps):
+      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the single-argument hue variant. The result
+      // is passed through strip.gamma32() to provide 'truer' colors
+      // before assigning to each pixel:
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+    }
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+  stripClear();
+}
+
+void theaterChaseRainbow(int wait) {
+  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
+  for(int a=0; a<20; a++) {  // Repeat 20 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in increments of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        // hue of pixel 'c' is offset by an amount to make one full
+        // revolution of the color wheel (range 65536) along the length
+        // of the strip (strip.numPixels() steps):
+        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
+        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show();                // Update strip with new contents
+      delay(wait);                 // Pause for a moment
+      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
+    }
+  }
+  stripClear();
+}
+
+
+void stripClear() {
+  for (int ii = 0; ii < strip.numPixels(); ++ii) {  
+    strip.setPixelColor(ii, 0, 0, 0);  
+  }
+  strip.show();
+}
+  
